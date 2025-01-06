2affda51463356204c26a84d580857af42bf19ff
m-hamza-aslam <hamza.prolink@gmail.com>
2025-01-06 17:50:47 +0500

    updated UpdateProject controller

--- EmployeeAdminPortal/Controllers/ProjectController.cs
+++ EmployeeAdminPortal/Controllers/ProjectController.cs
@@ -148,7 +148,10 @@ namespace EmployeeAdminPortal.Controllers
         [Route("{id}")]
         public IActionResult UpdateProject(int id, UpdateProjectDto updateProjectDto)
         {
-            var project = _dbContext.Projects.Find(id);
+            var project = _dbContext.Projects
+                .Include(p => p.Employees) // Include the navigation property
+                .FirstOrDefault(p => p.Id == id);
+
             if (project is null)
             {
                 return NotFound();
@@ -157,10 +160,31 @@ namespace EmployeeAdminPortal.Controllers
             project.Name = updateProjectDto.Name;
             project.Description = updateProjectDto.Description;
 
+            if (updateProjectDto.EmployeeIds is not null)
+            {
+                // Find the employees by their IDs
+                var employees = _dbContext.Employees
+                    .Where(e => updateProjectDto.EmployeeIds.Contains(e.Id))
+                    .ToList();
+
+                if (employees.Count != updateProjectDto.EmployeeIds.Count)
+                {
+                    return BadRequest("Some employees were not found in the database.");
+                }
+
+                // Update the project's employees
+                project.Employees.Clear();
+
+                foreach (var employee in employees)
+                {
+                    project.Employees.Add(employee);
+                }
+            }
+
             _dbContext.Projects.Update(project);
             _dbContext.SaveChanges();
 
-            return Ok(project);
+            return Ok();
         }
 
         [HttpDelete]
